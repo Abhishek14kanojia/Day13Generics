2751d805d2a9aff0afa25c8f9633413bd117c21b
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-12-07 23:52:04 +0530

    Integer

---
+++ Maximum.java
@@ -0,0 +1,38 @@
+package com.day13Generics;
+
+public class Maximum<T extends Number> {
+	Integer a;
+	Integer b;
+	Integer c;
+	
+	public void maximumNumber(Integer a, Integer b, Integer c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		if(a > b) {
+			if(a > c) {
+				System.out.println(a + "is Maximum");
+			}else {
+				System.out.println(c + "is Maximum");
+			}
+		}else if(b > c) {
+			System.out.println(b + "is Maximum");
+		}else {
+			System.out.println(c + "is Maximum");
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ MaximumMain.java
@@ -0,0 +1,8 @@
+package com.day13Generics;
+
+public class MaximumMain {
+	public static void main(String[] args) {
+		Maximum<Integer> obj = new Maximum<>();
+		obj.maximumNumber(10, 20, 30);
+	}
+}
