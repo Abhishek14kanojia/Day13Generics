ca2fa51069bc73eb36bbdc58e96eee915a847324
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-12-08 00:19:13 +0530

    String

--- Maximum.java
+++ Maximum.java
@@ -1,38 +1,23 @@
 package com.day13Generics;
 
-public class Maximum<T extends Number> {
-	Float a;
-	Float b;
-	Float c;
+public class Maximum {
+	String a;
+	String b;
+	String c;
 	
-	public void maximumNumber(Float a, Float b, Float c) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
-		
+	public static String maximumNumber(String a, String b, String c) {
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(c);
 		
-		if(a > b) {
-			if(a > c) {
-				System.out.println(a + "is Maximum");
-			}else {
-				System.out.println(c + "is Maximum");
-			}
-		}else if(b > c) {
-			System.out.println(b + "is Maximum");
-		}else {
-			System.out.println(c + "is Maximum");
-		}
-		
+		String maximum = a;
+		if(b.compareTo(maximum) > 0) {
+			maximum = b;
+		}if(c.compareTo(maximum) > 0) {
+			maximum = c;
+		}return maximum;
 		
 		
 	}
-	
-	
-	
-	
-	
 	
 }
