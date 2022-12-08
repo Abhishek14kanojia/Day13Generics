01657b705145e548137d4d96b6820e0406a10207
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-12-08 19:49:37 +0530

    Refactor

--- Maximum.java
+++ Maximum.java
@@ -1,23 +1,19 @@
 package com.day13Generics;
 
-public class Maximum {
-	String a;
-	String b;
-	String c;
+public class Maximum<T extends Comparable<T>> {
+	T a;
+	T b;
+	T c;
 	
-	public static String maximumNumber(String a, String b, String c) {
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-		
-		String maximum = a;
-		if(b.compareTo(maximum) > 0) {
-			maximum = b;
-		}if(c.compareTo(maximum) > 0) {
-			maximum = c;
-		}return maximum;
-		
-		
-	}
-	
+	public static<T extends Comparable<T>> T maximumNumber(T a, T b, T c){
+		{
+			T max = a;
+			if(b.compareTo(max) > 0) {
+				max = b;
+			}if(c.compareTo(max) > 0) {
+				max = c;
+			}
+			return max;
+		}
+	}		
 }
--- MaximumMain.java
+++ MaximumMain.java
@@ -3,7 +3,8 @@ package com.day13Generics;
 public class MaximumMain {
 	public static void main(String[] args) {
 	
-	System.out.println("Maximum String is " + Maximum.maximumNumber ("Apple", "Mango", "Watermelon"));
-	
+		System.out.println("Maximum Number is : " + Maximum.maximumNumber(50, 100, 125));	
+		System.out.println("Maximum Number is : " + Maximum.maximumNumber(46.50, 84.40, 85.90));
+		System.out.println("Maximum Number is : " + Maximum.maximumNumber("Abhishek", "Kumar", "Kanojia"));
 	}
 }
