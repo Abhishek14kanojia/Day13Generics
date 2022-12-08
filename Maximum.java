b3957d2815b1dd30ff35fa05cec99175b111d05e
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-12-08 20:19:19 +0530

    UC4

--- Maximum.java
+++ Maximum.java
@@ -1,32 +1,23 @@
 package com.day13Generics;
 
+import java.util.Arrays;
+
 public class Maximum<T extends Comparable<T>> {
-	T a;
-	T b;
-	T c;
 	
+	T[] arr;
 	
-	public Maximum(T a, T b, T c) {
-		super();
-		this.a = a;
-		this.b = b;
-		this.c = c;
+	public void maxi() {
+		Integer a = arr.length;
+		T obj = arr[a - 1];
+		System.out.println(obj);
 	}
 	
-	public T maximum() {
-		return Maximum.maximumNumber(a, b, c);
+	public void getValue(T[] arr) {
+		this.arr = arr;
+		maxi();
 	}
-
-
-	public static<T extends Comparable<T>> T maximumNumber(T a, T b, T c){
-		{
-			T max = a;
-			if(b.compareTo(max) > 0) {
-				max = b;
-			}if(c.compareTo(max) > 0) {
-				max = c;
-			}
-			return max;
-		}
-	}		
+	
+	public<T extends Comparable<T>> void maximum() {
+		Arrays.sort(arr);
+	}
 }
--- MaximumMain.java
+++ MaximumMain.java
@@ -3,18 +3,21 @@ package com.day13Generics;
 public class MaximumMain {
 	public static void main(String[] args) {
 	
-		Integer a = 50 , b = 100, c = 125;
-		int maxInt = new Maximum<Integer>(a, b, c).maximum();
-		System.out.println("Maximum is : " + maxInt);
+	Integer [] intArray = {1, 2, 3, 4, 5};
+	Double [] doubleArray = {1.5, 2.5, 3.5, 4.5, 5.5};	
+	String [] stringArray = {"Abhishek", "Kumar", "kanojia", "Deepti", "Virendra"};
 	
-		Double a1 = 50.50, b1 = 100.50, c1 = 125.24;
-		double maxDouble = new Maximum<Double>(a1, b1, c1).maximum();
-		System.out.println("Maximum is : " + maxDouble);
-		
-		String a2 = "Abhishek", b2 = "Kumar", c2 = "Kanojia";
-		String maxString = new Maximum<String>(a2, b2, c2).maximum();
-		System.out.println("Maximum is : " + maxString);
-		
-		
+	Maximum<Integer> obj1 = new Maximum<>();
+	Maximum<Double> obj2 = new Maximum<>();
+	Maximum<String> obj3 = new Maximum<>();
+
+	obj1.getValue(intArray);
+	obj2.getValue(doubleArray);
+	obj3.getValue(stringArray);
+	
+	obj1.maxi();
+	obj2.maxi();
+	obj3.maxi();
+	
 	}
 }
